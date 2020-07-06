62fa67673dea6b53a3ad4db6c80c5fbeaff46c3a
Konstantyn Verdysh <verdysh1991@gmail.com>
2020-07-06 21:29:27 +0300

    Fix commit

--- src/main/java/com/ua/verdysh/controller/AdvertisementParser.java
+++ src/main/java/com/ua/verdysh/controller/AdvertisementParser.java
@@ -7,6 +7,15 @@ import org.jsoup.nodes.Document;
 import java.util.List;
 
 class AdvertisementParser implements Parsable {
+
+    private static final String PRICE_USD_SELECTOR = "div.price_value > strong";
+    private static final String PRICE_UAH_SELECTOR = "div.price_value--additional > span:nth-child(3) > span";
+    private static final String HEADING_SELECTOR = "h1";
+    private static final String CITY_SELECTOR = "section.seller.proven:nth-child(2) ul.checked-list.unstyle.mb-15 li.item:nth-child(1) > div.item_inner";
+    private static final String SELLER_SELECTOR = "h4";
+    private static final String PHOTO_SELECTOR = "div.photo-620x465 > picture > img";
+    private static final String DESCRIPTION_SELECTOR = "dd[class='additional-data show-line'] div#full-description";
+    private static final String ID_SELECTOR = "section.m-padding.mb-20 ul.mb-10-list.unstyle.size13.mb-15 li:nth-child(2) span";
     private Document document;
 
     AdvertisementParser(Document document) {
@@ -17,36 +26,44 @@ class AdvertisementParser implements Parsable {
     public String parseUrl() {
         return document.location();
     }
+
     @Override
     public String parsePriceUsd() {
-        return ParserHelper.getElementText(document, "div.price_value > strong");
+        return ParserHelper.getElementText(document, PRICE_USD_SELECTOR);
     }
+
     @Override
     public String parsePriceUah() {
-        return  ParserHelper.getElementText(document, "div.price_value--additional > span:nth-child(3) > span");
+        return  ParserHelper.getElementText(document, PRICE_UAH_SELECTOR);
     }
+
     @Override
     public String parseHeading() {
-        return document.select("h1").attr("title");
+        return document.select(HEADING_SELECTOR).attr("title");
     }
+
     @Override
     public String parseCity() {
-        return ParserHelper.getElementText(document, "section.seller.proven:nth-child(2) ul.checked-list.unstyle.mb-15 li.item:nth-child(1) > div.item_inner");
+        return ParserHelper.getElementText(document, CITY_SELECTOR);
     }
+
     @Override
     public String parseSeller() {
-        return ParserHelper.getElementText(document, "h4");
+        return ParserHelper.getElementText(document, SELLER_SELECTOR);
     }
+
     @Override
     public List<String> parsePhoto() {
-        return document.select("div.photo-620x465 > picture > img").eachAttr("src");
+        return document.select(PHOTO_SELECTOR).eachAttr("src");
     }
+
     @Override
     public String parseDescription() {
-        return ParserHelper.getElementText(document, "dd[class='additional-data show-line'] div#full-description");
+        return ParserHelper.getElementText(document, DESCRIPTION_SELECTOR);
     }
+
     @Override
     public String parseId() {
-        return ParserHelper.getElementText(document,"section.m-padding.mb-20 ul.mb-10-list.unstyle.size13.mb-15 li:nth-child(2) span");
+        return ParserHelper.getElementText(document,ID_SELECTOR);
     }
 }
--- src/main/java/com/ua/verdysh/controller/DealerAdvertisementParser.java
+++ src/main/java/com/ua/verdysh/controller/DealerAdvertisementParser.java
@@ -7,6 +7,13 @@ import org.jsoup.nodes.Document;
 import java.util.List;
 
 class DealerAdvertisementParser implements Parsable {
+
+    private static final String PRICE_USD_SELECTOR = "section[class='price mb-15 mhide'] div.price_value";
+    private static final String PRICE_UAH_SELECTOR = "div[class='price_value price_value--additional']";
+    private static final String HEADING_SELECTOR = "h1";
+    private static final String CITY_SELECTOR = "ul.checked-list.unstyle:nth-child(2) li.item:nth-child(1) > div.item_inner";
+    private static final String SELLER_SELECTOR = "section[class='seller mb-15'] div.seller_info_area strong";
+    private static final String PHOTO_SELECTOR = ".image-gallery-slides img";
     private Document document;
 
     DealerAdvertisementParser(Document document) {
@@ -17,34 +24,42 @@ class DealerAdvertisementParser implements Parsable {
     public String parseUrl() {
         return document.location();
     }
+
     @Override
     public String parsePriceUsd() {
-        return ParserHelper.getElementText(document, "section[class='price mb-15 mhide'] div.price_value");
+        return ParserHelper.getElementText(document, PRICE_USD_SELECTOR);
     }
+
     @Override
     public String parsePriceUah() {
-        return ParserHelper.getElementText(document, "div[class='price_value price_value--additional']");
+        return ParserHelper.getElementText(document, PRICE_UAH_SELECTOR);
     }
+
     @Override
     public String parseHeading() {
-        return ParserHelper.getElementText(document, "h1");
+        return ParserHelper.getElementText(document, HEADING_SELECTOR);
     }
+
     @Override
     public String parseCity() {
-        return ParserHelper.getElementText(document, "ul.checked-list.unstyle:nth-child(2) li.item:nth-child(1) > div.item_inner");
+        return ParserHelper.getElementText(document, CITY_SELECTOR);
     }
+
     @Override
     public String parseSeller() {
-        return ParserHelper.getElementText(document, "section[class='seller mb-15'] div.seller_info_area strong");
+        return ParserHelper.getElementText(document, SELLER_SELECTOR);
     }
+
     @Override
     public List<String> parsePhoto() {
-        return document.select(".image-gallery-slides img").eachAttr("src");
+        return document.select(PHOTO_SELECTOR).eachAttr("src");
     }
+
     @Override
     public String parseDescription() {
         return "";
     }
+
     @Override
     public String parseId() {
         return "";
